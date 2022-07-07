2ad9e322eca99323531d7bc11f94086c546af309
Vadym Karachenko <v1dr00x@gmail.com>
2022-07-07 15:57:50 -0700

    Fixed startup errors

--- FitBuddy/src/main/java/com/example/FitBuddy/Authentication/AuthenticationController.java
+++ FitBuddy/src/main/java/com/example/FitBuddy/Authentication/AuthenticationController.java
@@ -1,25 +1,15 @@
 package com.example.FitBuddy.Authentication;
 
 
-import com.example.FitBuddy.Entity.Activities;
 import com.example.FitBuddy.Entity.Registration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping(path = "fitbuddy/auth")
 public class AuthenticationController {
-
-
-    private final AuthenticationService service;
-
     @Autowired
-    public AuthenticationController(AuthenticationService service) {
-        this.service = service;
-    }
+    private AuthenticationService service;
 
     @PostMapping(path = "/register")
     public void registerNewUser(@RequestBody Registration userRegistrationDetails)
