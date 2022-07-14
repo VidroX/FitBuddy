9c1c97f8861cbb33a36802eb62ada4377efb9b14
Mohibullah113 <mohibullahahsan30@gmail.com>
2022-07-14 12:14:04 -0700

    test_login
    v1

--- backend/app/tests/test_login.py
+++ backend/app/tests/test_login.py
@@ -1,12 +1,33 @@
+from fastapi import Form
 import pytest
 import json
 import requests
 
 
-def test_valid_login(url):
+def login_url():
+    return "http://localhost:8000/api/v1/auth/login"
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_login_page_open():
     url = "http://localhost:3000/auth/login"
-    data = {'email': 'test@example.com', 'password': 'TEST123#'}
-
-    response = requests.get(url, data=data)
-
+    response = requests.get(url)
     assert response.status_code == 200
+
+
+# @pytest.mark.skip(reason="no way of currently testing this")
+@pytest.mark.parametrize("input_credentials, expected",
+                         [({"email": "test@example.com", "password": "TEST123#"}, 200),
+                          ({"email": "test", "password": "TEST123#"}, 400),
+                          ({"email": ".com", "password": "TEST123#"}, 400),
+                          ({"email": "@example", "password": "TEST123#"}, 400),
+                          ({"email": "", "password": "TEST123#"}, 400),
+                          ({"email": "", "password": ""}, 400),
+                          ({"email": "", "password": "TEST123#"}, 400),
+                          ({"email": "test@example.com", "password": ""}, 400)])
+def test_login(input_credentials, expected):
+
+    url = login_url()
+    data = input_credentials
+    response = requests.post(url, data=data)
+    assert response.status_code == expected
--- backend/app/tests/test_registration.py
+++ backend/app/tests/test_registration.py
@@ -6,7 +6,14 @@ import requests
 
 
 def url():
-    return ""
+    return "http://localhost:8000/api/v1/auth/register"
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_registeration_page_open():
+    url = "http://localhost:3000/auth/register"
+    response = requests.get(url)
+    assert response.status_code == 200
 
 
 def test_valid_registration():
