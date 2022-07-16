4e6c3e1e4717ef35f8d959f653bcd8a2b79cf2f2
Mohibullah113 <mohibullahahsan30@gmail.com>
2022-07-15 20:25:26 -0700

    Tests

--- backend/app/tests/test_registration.py
+++ backend/app/tests/test_registration.py
@@ -1,12 +1,25 @@
 # test_registration
 
+import io
+import logging
+from pyparsing import Literal
 import pytest
 import json
 import requests
+from fastapi import UploadFile
+import pytest_asyncio
+from app.main import app
+from fastapi.testclient import TestClient
+import os
 
 
-def url():
-    return "http://localhost:8000/api/v1/auth/register"
+def register_url():
+    return "/api/v1/auth/register"
+
+
+async def create_upload_file(file: UploadFile):
+    print(file)
+    return {"images": file.filename}
 
 
 @pytest.mark.skip(reason="no way of currently testing this")
@@ -16,9 +29,24 @@ def test_registeration_page_open():
     assert response.status_code == 200
 
 
-def test_valid_registration():
-    url = url()
-    data = {'firstName': '', 'lastName': '', 'email': '', 'password': '',
-            'about': '', 'images': '', 'activities': '', 'gender': '', 'address': ''}
-    response = requests.get(url, data=data)
-    assert response.status_code == 200
+#client = TestClient(app)
+
+
+async def test_valid_registration():
+    with TestClient(app) as client:
+
+        url = register_url()
+
+        files = {'images': open("Sunflower_from_Silesia2.jpg", "rb")}
+
+        data = {'firstname': 'test',
+                'lastname': 'user',
+                'email': 'test_user2@gmail.com',
+                'password': 'TEST123#',
+                'about': 'asdads',
+                'gender': 'M',
+                'activities': ["62ce58147f0497ee25cecafd"],
+                'address': 'dsfsdfs'}
+
+        response = client.post(url, data=data, files=files)
+        assert response.status_code == 200
