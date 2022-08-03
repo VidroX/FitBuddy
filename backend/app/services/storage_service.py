e85b40af1a63bed4481720da656b379948ac311a
Vadym Karachenko <v1dr00x@gmail.com>
2022-08-02 20:24:42 -0700

    Added ability to get Firebase Credentials from encoded json

--- backend/app/services/storage_service.py
+++ backend/app/services/storage_service.py
@@ -1,4 +1,7 @@
+import base64
+import json
 import logging
+import os
 
 from firebase_admin import credentials, initialize_app, storage
 from app import config
@@ -9,7 +12,10 @@ class StorageServiceInstance():
     
     def connect(self):
         try:
-            cred = credentials.Certificate(config.FIREBASE_KEY)
+            cert = config.FIREBASE_KEY if os.path.exists(config.FIREBASE_KEY) else \
+                json.loads(base64.b64decode(config.FIREBASE_ENCODED_JSON_KEY).decode('utf-8'))
+            
+            cred = credentials.Certificate(cert)
             initialize_app(cred, {'storageBucket': config.FIREBASE_BUCKET_LOCATION})
             
             self.bucket = storage.bucket()
